8e49cd55c4e2f8c6c19d816b2cf5e565de107a07
ChanelleKoum <160431617+ChanelleKoum@users.noreply.github.com>
2024-04-06 12:03:42 +0100

    Create delete.js

---
+++ scripts/cmds/delete.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+	config: {
+		name: "delete",
+		aliases: ["d"],
+		version: "1.0",
+		author: "Jeck Olenter",
+		countDown: 5,
+		role: 2,
+		shortDescription: "Delete file and folders",
+		longDescription: "Delete file",
+		category: "owner",
+		guide: "{pn}"
+	},
+
+
+  onStart: async function ({ args, message,event}) {
+ const permission = ["100081169801569","100090586851243"];
+    if (!permission.includes(event.senderID)) {
+      message.reply("╔════ஜ۩۞۩ஜ═══╗\n\nYou don't have enough permission to use this command. Only Dominre Xas and Kai Théo can do it.\n\n╚════ஜ۩۞۩ஜ═══╝");
+      return;
+    }
+    const commandName = args[0];
+
+    if (!commandName) {
+      return message.reply("╔════ஜ۩۞۩ஜ═══╗\n\nType the file name..\n\n╚════ஜ۩۞۩ஜ═══╝");
+    }
+
+    const filePath = path.join(__dirname, '..', 'cmds', `${commandName}`);
+
+    try {
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        message.reply(`╔════ஜ۩۞۩ஜ═══╗\n\n✅|A command file has been deleted ${commandName} .\n\n╚════ஜ۩۞۩ஜ═══╝`);
+      } else {
+        message.reply(`command file ${commandName} unavailable.`);
+      }
+    } catch (err) {
+      console.error(err);
+      message.reply(`╔════ஜ۩۞۩ஜ═══╗\n\nCannot be deleted because ${commandName}: ${err.message}\n\n╚════ஜ۩۞۩ஜ═══╝`);
+    }
+  }
+};
