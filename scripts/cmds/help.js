d493ae101eec0f6bc2c8b91567107dbffebf8ba1
ChanelleKoum <160431617+ChanelleKoum@users.noreply.github.com>
2024-04-06 12:45:46 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🍭 | 🍻~𝗗𝗼𝗺𝗶𝗻𝗿𝗲𝗕𝗼𝘁 𝗩𝟮~🍻 ]";
+const doNotDelete = "[ 🍭 |🍻~𝗗𝗼𝗺𝗶𝗻𝗿𝗲𝗕𝗼𝘁 𝗩𝟮~🍻 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
